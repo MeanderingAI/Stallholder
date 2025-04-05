488303055be6ab7243ac23e909e1e5facfa7d698
Mehran Ghamaty <mnghamaty@gmail.com>
2025-04-05 13:35:08 -0700

    Added file and folder handler to library

---
+++ src/main/java/stallholder/Handlers/FileHandler.java
@@ -0,0 +1,44 @@
+package stallholder.Handlers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import stallholder.ContentType;
+import stallholder.MyHttpRequest;
+import stallholder.MyHttpResponse;
+import stallholder.RequestHandler;
+import stallholder.StatusCodes;
+
+public class FileHandler extends RequestHandler{
+    File file;
+    String file_path;
+    byte[] file_data;
+    public FileHandler(String file_path) throws FileNotFoundException, IOException {
+        super();
+        this.file_path = file_path;
+        this.file = new File(this.file_path);
+        
+        if(!this.file.exists()) {
+            throw new RuntimeException("File does not exist");
+        }
+        if(this.file.isDirectory()) {
+            throw new RuntimeException("FileHandler cannot handle directories");
+        }
+        this.file_data = new byte[(int) file.length()];
+        try(FileInputStream fileInputStream = new FileInputStream(file)) {
+            fileInputStream.read(this.file_data);
+        }
+    }
+
+    @Override
+    public MyHttpResponse HandleRequest(MyHttpRequest request) {
+        MyHttpResponse response = new MyHttpResponse();
+        ContentType content_type = ContentType.fromFileName(file.getName());
+        response.SetCode(StatusCodes.OK);
+        response.SetContent(this.file_data);
+        response.SetContentType(content_type);
+        return response;
+    }
+}
---
+++ src/main/java/stallholder/Handlers/FolderHandler.java
@@ -0,0 +1,60 @@
+package stallholder.Handlers;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import stallholder.ContentType;
+import stallholder.MyHttpRequest;
+import stallholder.MyHttpResponse;
+import stallholder.RequestHandler;
+import stallholder.StatusCodes;
+import stallholder.Utils;
+
+public class FolderHandler extends RequestHandler {
+    String folder_path;
+    public FolderHandler(String folder_path) {
+        super();
+        this.folder_path = folder_path;
+    }
+
+
+    public byte[] readFileToByteArray(File file) throws IOException {
+        byte[] fileData = new byte[(int) file.length()];
+        try(FileInputStream fileInputStream = new FileInputStream(file)) {
+            fileInputStream.read(fileData);
+        }
+        return fileData;
+    }
+
+    @Override
+    public MyHttpResponse HandleRequest(MyHttpRequest request) throws IOException {
+        MyHttpResponse response = new MyHttpResponse();
+
+        String file_name = Utils.getLastTokenInPath(request.getRequestURL());
+        String full_path = this.folder_path + "/" + file_name;
+        File f = new File(full_path);
+        if(f.exists()) {
+            if(f.isDirectory()) {
+                response.SetCode(StatusCodes.FORBIDDEN);
+                response.SetContent("Directory access is forbidden", ContentType.PLAIN);
+                return response;
+            } else {
+                response.SetCode(StatusCodes.OK);
+                response.SetContent(Files.readAllBytes(Paths.get(full_path)));
+                ;
+                response.SetContentType(ContentType.fromFileName(file_name));
+            }
+            
+        } else {
+            response.SetCode(StatusCodes.NOT_FOUND);
+            response.SetContent("File not found", ContentType.PLAIN);
+        }
+
+
+        return  response;
+    }
+}
+
